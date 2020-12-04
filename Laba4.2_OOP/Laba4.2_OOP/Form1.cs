f644e7c9977feceb0514a828d3df171c3b52cb09
Khavanova Olga <khavanova@mail.ru>
2020-12-04 23:58:20 +0500

    4. Приложение с использованием идей MVC Создание сlass Model, необходимых функций для обработки и обновления значения компонента на форме + комментарии

--- Laba4.2_OOP/Laba4.2_OOP/Form1.cs
+++ Laba4.2_OOP/Laba4.2_OOP/Form1.cs
@@ -12,63 +12,56 @@ namespace Laba4._2_OOP
 {
     public partial class MainForm : Form
     {
+        Model model; //Ссылки на
+        Model model1; //модель
         public MainForm()
         {
             InitializeComponent();
+            model = new Model(); //Создаем модель 1-й группы
+            //Обновление компонент 1
+            model.observers += new System.EventHandler(this.UpdateFromModel);
+            model1 = new Model(); //Создаем модель 2-ой группы
+            //Обновление компонент 2
+            model1.observers += new System.EventHandler(this.UpdateFromModel1);
+            model1.setValue(30);
         }
 
         //Обработчик события "Изменение значения компонента"
         private void numericUpDown1_ValueChanged(object sender, EventArgs e)
         {
-            //Проверка значения на четность
-            if (numericUpDown1.Value % 2 == 1)
-                numericUpDown1.Value = numericUpDown1.Value + 1;
-            //Обновляем вручную значения всех остальных компонентов
-            progressBar1.Value = Decimal.ToInt32(numericUpDown1.Value);
-            textBox1.Text = numericUpDown1.Value.ToString();
+            //При изменении значения компонента, передаем его в модель
+            model.setValue(Decimal.ToInt32(numericUpDown1.Value));      
         }
 
         //Обработчик события "Нажатие на клавишу"
         private void textBox1_KeyDown(object sender, KeyEventArgs e)
         {
-            //При нажатии на клавишу "Enter"
             if (e.KeyCode == Keys.Enter)
-                //Проверка значения на четность
-                if (Int32.Parse(textBox1.Text) % 2 == 1)
-                    textBox1.Text = (Int32.Parse(textBox1.Text) + 1).ToString();
-            //Обновляем вручную значения всех остальных компонентов
-            numericUpDown1.Value = Int32.Parse(textBox1.Text);
-            progressBar1.Value = Int32.Parse(textBox1.Text);
+            { 
+                model.setValue(Int32.Parse(textBox1.Text)); 
+            }
 
         }
         //Обработчик события "Перемещение курсора мыши по компоненту"
         private void progressBar1_MouseMove(object sender, MouseEventArgs e)
         {
-            progressBar1.Value = (int)(e.X * 5.586592);
+            model.setValue((int)(e.X * 5.586592));
 
-            //Проверка значения на четность
-            if (progressBar1.Value%2==1)
-            {
-                //Обновляем вручную значения всех остальных компонентов
-                textBox1.Text = (progressBar1.Value + 1).ToString();
-                numericUpDown1.Value = progressBar1.Value + 1;
-            }
+        }
 
-            else
-            {
-                //Обновляем вручную значения всех остальных компонентов
-                textBox1.Text = (progressBar1.Value).ToString();
-                numericUpDown1.Value = progressBar1.Value;
-            }
+        //Обновление значений компонент 1-ой группы на форме
+        private void UpdateFromModel(object sender, EventArgs e)
+        {
+            textBox1.Text = model.getValue().ToString();
+            numericUpDown1.Value = model.getValue();
+            progressBar1.Value = model.getValue();
         }
 
         //Обработчик события "Изменение значения компонента"
         private void numericUpDown2_ValueChanged(object sender, EventArgs e)
         {
-            numericUpDown2.Value = numericUpDown2.Value;
-            //Обновляем вручную значения всех остальных компонентов
-            button1.Size = new System.Drawing.Size (Decimal.ToInt32(numericUpDown2.Value), Decimal.ToInt32(numericUpDown2.Value));
-            textBox2.Text = numericUpDown2.Value.ToString();
+            //При изменении значения компонента, передаем его в модель
+            model1.setValue1(Decimal.ToInt32(numericUpDown2.Value));
         }
 
         //Обработчик события "Нажатие на клавишу"
@@ -77,20 +70,54 @@ namespace Laba4._2_OOP
             //При нажатии на клавишу "Enter"
             if (e.KeyCode == Keys.Enter)
             {
-                textBox2.Text = (Int32.Parse(textBox2.Text)).ToString();
-                //Обновляем вручную значения всех остальных компонентов
-                numericUpDown2.Value = Int32.Parse(textBox2.Text);
-                button1.Size = new System.Drawing.Size(Int32.Parse(textBox2.Text), Int32.Parse(textBox2.Text));
+                //При изменении значения компонента, передаем его в модель
+                model1.setValue1(Int32.Parse(textBox2.Text));
             }              
         }
 
         //Обработчик события "Нажатие на компонент"
         private void button1_Click(object sender, EventArgs e)
         {
-            button1.Size = new System.Drawing.Size(this.button1.Width + 1, this.button1.Height + 1);
-            //Обновляем вручную значения всех остальных компонентов
-            textBox2.Text = button1.Width.ToString();
-            numericUpDown2.Value = button1.Width;
+            //При изменении значения компонента, передаем его в модель
+            model1.setValue1((model1.getValue() + 1));
+        }
+
+        //Обновление значений компонент 2-ой группы на форме
+        private void UpdateFromModel1(object sender, EventArgs e)
+        {
+            textBox2.Text = model1.getValue().ToString();
+            numericUpDown2.Value = model1.getValue();
+            button1.Size = new System.Drawing.Size(model1.getValue(), model1.getValue());
+        }
+
+    }
+
+    public class Model
+    {
+        private int value; //Значения компонента
+        public System.EventHandler observers; //Событие
+
+        //Функция для изменения значения компонента 1-ой группы(четные)
+        public void setValue(int value)
+        {
+            if (value % 2 == 1)
+                this.value = value + 1;
+            else
+                this.value = value;
+            observers.Invoke(this, null); //Вызов обновления
+        }
+
+        //Функция для изменения значения компонента 2-ой группы(любые)
+        public void setValue1(int value)
+        {
+                this.value = value;
+            observers.Invoke(this, null);
+        }
+
+        //Функция для возвращения значения компонента при обновлении
+        public int getValue()
+        {
+            return value;
         }
     }
 }
